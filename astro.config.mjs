74200b2a708c16111576e512d62f927634566db9
Alan <120235005+AlanGuillenZ@users.noreply.github.com>
2025-11-20 15:55:15 -0600

    Remove cloudflare adapter dependency

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,6 @@
 import { defineConfig } from 'astro/config';
-import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   site: 'https://AlanGuillenZ.github.io',
   base: '/example1/',
-  adapter: cloudflare(),
 });
