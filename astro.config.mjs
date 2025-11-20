b0125fa7f564b345cdf90ff384bb72b2c95aba45
Alan Guillen <alanzuniga012305@gmail.com>
2025-11-20 13:45:32 -0600

    add @astrojs/cloudflare dependency to package.json again

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
 import { defineConfig } from 'astro/config';
+import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   site: 'https://AlanGuillenZ.github.io',
   base: '/example1/',
-  output: 'static',
+  adapter: cloudflare(),
 });
