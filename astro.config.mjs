236316d6092f1f490272010c55cf1be1b0acfcaf
Alan Guillen <alanzuniga012305@gmail.com>
2025-11-20 13:42:09 -0600

    Remove Cloudflare dependency and delete wrangler configuration file

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,7 @@
 import { defineConfig } from 'astro/config';
 
-import cloudflare from '@astrojs/cloudflare';
-
 export default defineConfig({
   site: 'https://AlanGuillenZ.github.io',
   base: '/example1/',
-  adapter: cloudflare(),
+  output: 'static',
 });
